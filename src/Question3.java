0d3be148206d7ce691ae484d2551144a79f3e8c6
MehtaShivam <smm2zr@virginia.edu>
2020-09-11 13:30:43 -0400

    Fib

--- src/Question3.java
+++ src/Question3.java
@@ -3,18 +3,19 @@ public class Question3 {
 	public static void main(String[] args) {
 		int arg = Integer.parseInt(args[0]);
 		
-		while(arg >= 0) {
-			if (arg == 0) {
-				System.out.println(arg);
-				break;
-			}else if(arg == 1) {
-				System.out.println(arg);
-				break;
-			}else {
-				System.out.println((arg-1)+(arg-2));
-				arg--;
-			}
+		int previousNumber = 0;
+		int nextNumber = 1;
+		 
+
+	    for (int i = 1; i <= arg; ++i)
+	        {
+	            System.out.print(previousNumber+" ");
+
+	            int sum = previousNumber + nextNumber;
+	            previousNumber = nextNumber;
+	            nextNumber = sum;
+	        }
 		}
 		
 	}
-}
+
